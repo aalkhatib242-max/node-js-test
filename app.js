fd462a1b7b0f207aaefabe73da783a4321a94ec8
aalkhatib242-max <aalkhatib242@gmail.com>
2025-10-20 19:51:36 +0300

    update routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express()
 app.set("view engine","ejs")
 app.set("views",__dirname + "/views")
 const mongoose=require("mongoose")
-const Article=require("./models/Article")
+const User=require("./models/User")
 mongoose.connect("mongodb+srv://AliAlkhatib10:AliAlkhatib2003@myfirstcluster.r46sloy.mongodb.net/?retryWrites=true&w=majority&appName=MyFirstCluster")
 .then(()=>{ 
 console.log("connected successfully")
@@ -16,83 +16,40 @@ const port=3000
 app.get("/",(req,res)=>{
     res.render("Articles.ejs")
 })
-app.get("/hello",(req,res)=>{
-    res.json({
-        name:req.body.name,
-        age:req.query.age
-    })
-})
-app.get("/1",(req,res)=>{
-    let num=""
-    for (let i=0;i<=100;i++){
-    num+=i+" "
-    
+const users=[]
+app.post('/register', async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        const  findUser=users.find((data)=>email==data.email)
+        if (findUser){
+            res.status(400).send("wrong email or password!")
+        }
+        const hashedPassword=await bcrypt.hash(password,10)
+        users.push({email,password:hashedPassword})
+        console.log(users)
+        res.status(201).send('Registered successfully')
+    }   catch (err) {
+        res.status(500).send({message:err.message}) 
+        }
     }
-    res.send(`the numbers are : ${num}`)
-})
-app.get("/2/:num1/:num2",(req,res)=>{
-    let num1=req.params.num1
-    let num2=req.params.num2
-    let total=Number(num1)+Number(num2)
-    res.send(`the total is : ${total}`)
-})
-app.get("/3",(req,res)=>{
-    let num1=req.body.num1
-    let num2=req.body.num2
-    let total=Number(num1)+Number(num2)
-    // res.send(`the total is : ${total}`)
-res.render("index.ejs",{
-    total:total
-})
-})
-app.get("/4",(req,res)=>{
-    let num1=req.query.num1
-    let num2=req.query.num2
-    let total=Number(num1)+Number(num2)
-    res.send(`the total is : ${total} and your name is : ${req.body.name}`)
-})
-app.get("/5",(req,res)=>{
-    
-    // res.sendFile(__dirname+"/html/index.html")
-    res.render("index.ejs",{
-        name:"Alloush",
-    })
-})
-app.post("/add",(req,res)=>{
-    res.send("post request on add")
-})
-app.delete("/delete_me",(req,res)=>{
-    res.send("visiting delete request")
-})
+)
+app.post("/LogIn", async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        const findUser=users.find((data)=>email==data.email)
+        if(!findUser){
+            res.status(400).send('wrong email or password !')
+        }
+        const passwordMatch=await bcrypt.compare(password,findUser.password)
+        if (passwordMatch){
+            res.status(200).send('logged successfully')
+        }
+        else{
+            res.status(400).send('wrong email or password !')
+        }
 
-
-app.post("/Articles",async (req,res)=>{
-    const newArticle=new Article()
-    const artTitle=req.body.articleTitle
-    const artBody=req.body.articleBody
-    res.send(artTitle+" "+artBody)
-
-    newArticle.title=artTitle
-    newArticle.body=artBody
-    newArticle.numberOfLikes=0
-    await newArticle.save()
-    res.json(newArticle)
+    }catch(err){res.status(500).send({message:err.message})}
 })
-app.get("/Articles",async(req,res)=>{
-    const articles=await Article.find()
-    res.json(articles)
-})
-app.delete("/Articles/:aa",async(req,res)=>{
-    const id=req.params.aa
-    const delete_me=await Article.findByIdAndDelete(id)
-    res.json(id)
-})
-// app.get("/showArticles",async(req,res)=>{
-//     const Articles=await Article.find()
-//     res.render("Articles.ejs",{
-//         AllArticles:Articles
-//     })
-// })
 app.listen(port,()=>{
     console.log(`server is running in port ${port}`)
 })
---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema
+
+const articleSchema=new Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+})
+
+const User=mongoose.model("User",userSchema)
+
+module.exports=User
+
+//  <% for( let art of AllArticles:) { %>
+// <%=art.body %>
+// <% } %>
+// <%= art.title%>
